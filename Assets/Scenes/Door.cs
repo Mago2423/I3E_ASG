e689f1295e448e0deb593c64372315e1cde1bb25
Mago2423 <weijun2423@gmail.com>
2026-06-12 01:09:46 +0800

    automatic door close code

--- Assets/Scenes/Door.cs
+++ Assets/Scenes/Door.cs
@@ -4,6 +4,23 @@ public class Door : MonoBehaviour
 {
     public Vector3 rotateAmount = new Vector3(0, 90, 0);
     bool isOpen = false;
+    int time = 1000;
+    bool isColliding = false;
+
+    void Update()
+    {
+        if (!isColliding && isOpen)
+        {
+            print("Countdown to close door");
+            time -= 1;
+            if (time <= 0 && !isColliding)
+            {
+                print("Closing door");
+                Close();
+                time = 1000;
+            }
+        }
+    }
     public int Interact()
     {
         var animator = GetComponent<Animator>();
@@ -17,5 +34,15 @@ public class Door : MonoBehaviour
         isOpen = false;
         animator.SetBool("IsOpen", isOpen);
         return 0;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        print($"Collided with {collision.gameObject.name}");
+        isColliding = true;
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        isColliding = false;
+        print($"Stopped colliding with {collision.gameObject.name}");
     }
 }
