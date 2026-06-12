164755d3674e3bbc4de8f0865a4b72656f27e016
Mago2423 <weijun2423@gmail.com>
2026-06-12 19:26:35 +0800

    AI Modified code for raycasting

--- Assets/Scenes/CollisionDetector.cs
+++ Assets/Scenes/CollisionDetector.cs
@@ -16,92 +16,95 @@ public class CollisionDetector : MonoBehaviour
 
     public int coinsCollected = 0;
 
-    GameObject currentCollider;
-    
     bool isMenuShowing = false;
     public UIManagerScript UIManagerScript;
     void OnMenu()
     {
         UIManagerScript.TogglePanel();
     }
-    void OnCollisionEnter(Collision collision)
-    {
-        currentCollider = collision.gameObject;
-        print($"Collided with {currentCollider.name}");
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        print($"Stopped colliding with {currentCollider.name}");
-        currentCollider = null;
-    }
 
     void OnInteract(InputValue value)
     {
         print("Interacted");
-        if (currentCollider != null && currentCollider.CompareTag("Item"))
+        
+        // Cast a ray forward from the player
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        
+        // Check if ray hits something within 5 units
+        if (Physics.Raycast(ray, out hit, 5f))
         {
-            var Collectible = currentCollider.GetComponent<Collectible>();
-            if (Collectible != null)
-            {
-                var collider = Collectible.GetComponent<Collider>();
-                if(collider != null && !collider.enabled)
-                {
-                    print($"Already collected {currentCollider.name}");
-                }
-                else
-                {
-                    print($"Interacted with {currentCollider.name}");
-                    itemsCollected += 1;
-                    Collectible.Collect();
-                    UIManagerScript.ItemCollected(itemsCollected);
-                }
-            }
-        }
-        else if (currentCollider != null)
-        {
-            print($"Interacted with {currentCollider.name}");
-            var Collectible = currentCollider.GetComponent<Collectible>();
-            if (Collectible != null)
-            {   
-                var collider = Collectible.GetComponent<Collider>();
-                if(collider != null && !collider.enabled)
-                {
-                    print($"Already collected {currentCollider.name}");
-                }
-                else
-                {
-                    print($"Interacted with {currentCollider.name}");
-                    coinsCollected += 1;
-                    score += Collectible.score;
-                    print($"Score: {score}");
-                    Collectible.Collect();
-                    UIManagerScript.UpdateScore(score);
-                    UIManagerScript.CoinsCollected(coinsCollected);
-                }
+            GameObject hitObject = hit.collider.gameObject;
+            print($"Raycasted hit: {hitObject.name}");
             
-            }
-            var Door = currentCollider.GetComponent<Door>();
-            if (Door != null)
+            if (hitObject.CompareTag("Item"))
             {
-                if (currentCollider.CompareTag("Locked"))
+                var Collectible = hitObject.GetComponent<Collectible>();
+                if (Collectible != null)
                 {
-                    if (itemsCollected >= 10)
+                    var collider = Collectible.GetComponent<Collider>();
+                    if(collider != null && !collider.enabled)
                     {
-                        print("Door is now unlocked!");
+                        print($"Already collected {hitObject.name}");
                     }
                     else
                     {
-                        print("Door is locked. Collect all items to unlock.");
-                        return; // Exit the method to prevent interaction with the door
+                        print($"Interacted with {hitObject.name}");
+                        itemsCollected += 1;
+                        Collectible.Collect();
+                        UIManagerScript.ItemCollected(itemsCollected);
                     }
                 }
-                else
+            }
+            else
+            {
+                print($"Interacted with {hitObject.name}");
+                var Collectible = hitObject.GetComponent<Collectible>();
+                if (Collectible != null)
+                {   
+                    var collider = Collectible.GetComponent<Collider>();
+                    if(collider != null && !collider.enabled)
+                    {
+                        print($"Already collected {hitObject.name}");
+                    }
+                    else
+                    {
+                        print($"Interacted with {hitObject.name}");
+                        coinsCollected += 1;
+                        score += Collectible.score;
+                        print($"Score: {score}");
+                        Collectible.Collect();
+                        UIManagerScript.UpdateScore(score);
+                        UIManagerScript.CoinsCollected(coinsCollected);
+                    }
+                
+                }
+                var Door = hitObject.GetComponent<Door>();
+                if (Door != null)
                 {
-                    print($"Interacted with {currentCollider.name}");
-                    Door.Interact();
+                    if (hitObject.CompareTag("Locked"))
+                    {
+                        if (itemsCollected >= 10)
+                        {
+                            print("Door is now unlocked!");
+                        }
+                        else
+                        {
+                            print("Door is locked. Collect all items to unlock.");
+                            return; // Exit the method to prevent interaction with the door
+                        }
+                    }
+                    else
+                    {
+                        print($"Interacted with {hitObject.name}");
+                        Door.Interact();
+                    }
                 }
             }
+        }
+        else
+        {
+            print("Nothing in range to interact with");
         }
     }
 
